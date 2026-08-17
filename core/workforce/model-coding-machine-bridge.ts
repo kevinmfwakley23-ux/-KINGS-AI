79500fa8bb1ff8d3f2992e0ded931a7f9f110217
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-17 01:09:01 -0600

    feat(workforce): bridge model output into governed coding work units

---
+++ core/workforce/model-coding-machine-bridge.ts
@@ -0,0 +1,90 @@
+import type {
+  ModelExecutionResult,
+} from "./model-interface";
+
+import {
+  GovernedLocalCodingProposal,
+} from "./local-coding-change-proposal";
+
+import {
+  ModelCodingProposalParser,
+  type ModelCodingProposalParserOptions,
+} from "./model-coding-proposal-parser";
+
+import type {
+  CodingWorkUnitExecutionRequest,
+} from "./coding-work-unit-execution";
+
+export interface ModelCodingMachineBridgeRequest {
+  modelResult:
+    ModelExecutionResult;
+
+  proposalParser:
+    ModelCodingProposalParserOptions;
+
+  execution:
+    Omit<
+      CodingWorkUnitExecutionRequest,
+      "proposal"
+    >;
+}
+
+export interface ModelCodingMachineBridgeResult {
+  request:
+    CodingWorkUnitExecutionRequest;
+
+  generatedProposalId:
+    string;
+}
+
+export class ModelCodingMachineBridge {
+  private readonly governedProposal =
+    new GovernedLocalCodingProposal();
+
+  buildRequest(
+    input:
+      ModelCodingMachineBridgeRequest,
+  ):
+    ModelCodingMachineBridgeResult {
+    const parser =
+      new ModelCodingProposalParser(
+        input.proposalParser,
+      );
+
+    const proposal =
+      this.governedProposal.propose({
+        response:
+          input.modelResult,
+        request: {
+          id:
+            `model-coding-request-${input.execution.taskId}`,
+          taskId:
+            input.execution.taskId,
+          missionId:
+            input.execution.projectId,
+          messages: [],
+          requiredCapabilities: [
+            "coding",
+          ],
+          inputModalities: [
+            "text",
+          ],
+          outputModality:
+            "text",
+          allowToolProposals:
+            false,
+        },
+        allowedPaths:
+          input.proposalParser.allowedPaths,
+      }, parser);
+
+    return {
+      request: {
+        ...input.execution,
+        proposal,
+      },
+      generatedProposalId:
+        proposal.id,
+    };
+  }
+}
